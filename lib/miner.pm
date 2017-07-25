8137cd33a227f4c2cd88ca0efed2dcf6408670ab
Todd Freed <todd.freed@gmail.com>
2017-07-24 20:16:10 -0700

    config: keys for miner path
    Allows the user to specify the full path to the miner executables with
    config parameters.
    
    Issue: #3
    Signed-off-by: Todd Freed <todd.freed@gmail.com>

--- lib/miner.pm
+++ lib/miner.pm
@@ -62,6 +62,7 @@ sub enumerate_miners
     my $name = $1;
 
     my @cmd = ($path, "configured");
+    miner_env_setup(miner => $_);
     my $output = filter(\@cmd);
 
     if($output eq "yes")
@@ -83,14 +84,9 @@ sub enumerate_algos
 {
   my $miner = shift;
 
-  my @cmd = (
-      $$miner{path}
-    , "algos"
-  );
+  miner_env_setup(miner => $$miner{name});
 
-  $ENV{autominer_path} = $$miner{path};
-
-  my $output = run(@cmd);
+  my $output = run($$miner{path}, "algos");
 
   for my $line (split(/\n/, $output))
   {
@@ -98,6 +94,7 @@ sub enumerate_algos
     {
       my $algo = normalize_algorithm($&);
 
+      # default to max speed to cause autominer to select algorithms for which there is no benchmark data
       $$miner{algos}{$algo} = { name => $algo, perf => [ ], speed => 0xffffffff };
     }
   }
@@ -105,14 +102,42 @@ sub enumerate_algos
 
 sub miner_env_setup
 {
-  my ($miner, $algo) = @_;
+  my %params = @_;
 
-  $ENV{AUTOMINER_MINER} = $$miner{name};
-  $ENV{AUTOMINER_ALGO} = $$algo{name};
-  $ENV{AUTOMINER_PORT} = nicehash::niceport($$algo{name});
-  $ENV{AUTOMINER_MARKET} = "nicehash";
-  $ENV{AUTOMINER_USERNAME} = $::opts{username};
+  $ENV{AUTOMINER_MINER} = $params{miner};
+
+  if($params{algo})
+  {
+    $ENV{AUTOMINER_ALGO} = $params{algo};
+    $ENV{AUTOMINER_PORT} = nicehash::niceport($params{algo});
+  }
+
+  $ENV{AUTOMINER_PAYOUT_ADDRESS} = $::opts{payout_address};
+  $ENV{AUTOMINER_USERNAME} = $::opts{payout_address};
+  $ENV{AUTOMINER_WORKER} = $::opts{worker};
+  $ENV{AUTOMINER_USERNAME} .= "." . $::opts{worker} if $::opts{worker};
   $ENV{AUTOMINER_CARDS_SPACES} = $::opts{"space-cards"};
   $ENV{AUTOMINER_CARDS_COMMAS} = $::opts{"comma-cards"};
   $ENV{AUTOMINER_PROFILE} = $::opts{profile};
+
+  if($params{market})
+  {
+    $ENV{AUTOMINER_MARKET} = "nicehash";
+    $ENV{AUTOMINER_NICEHASH_REGION} = "usa" if $params{market} eq "nicehash-usa";
+    $ENV{AUTOMINER_NICEHASH_REGION} = "eu" if $params{market} eq "nicehash-eu";
+  }
+
+  $ENV{AUTOMINER_CCMINER_PATH} = $::opts{ccminer_path};
+  $ENV{AUTOMINER_CCMINER_CRYPTONIGHT_PATH} = $::opts{"ccminer_cryptonight_path"};
+  $ENV{AUTOMINER_ETHMINER_PATH} = $::opts{ethminer_path};
+  $ENV{AUTOMINER_NHEQMINER_CUDA_PATH} = $::opts{nheqminer_cuda_path};
+  $ENV{AUTOMINER_SGMINER_PATH} = $::opts{sgminer_path};
+
+  my @env;
+  while(my($k, $v) = each %ENV)
+  {
+    push @env, "$k=$v" if substr($k, 0, 9) eq "AUTOMINER";
+  }
+  @env = sort @env;
+# print Dumper \@env;
 }
